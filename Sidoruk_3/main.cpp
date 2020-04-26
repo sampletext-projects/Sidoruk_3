7557a486fdfa518c39f357d5093d986ebce6ac2f
sampletext32 <sampletext32@bk.ru>
2020-04-26 14:22:27 +0300

    Add project

---
+++ Sidoruk_3/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double y(double x)
+{
+	double t = x * x - log(1 + x) - 3;
+	return t;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	double x1, x2;
+	cout << "Введите первый конец отрезка: ";
+	cin >> x1;
+
+	cout << "Введите второй конец отрезка: ";
+	cin >> x2;
+	
+	double y1 = y(x1);
+	double y2 = y(x2);
+
+	if (y1 * y2 < 0)
+	{
+		cout << "Корень на отрезке" << endl;
+	}
+	else
+	{
+		cout << "Корень вне отрезка" << endl;;
+	}
+
+	system("pause");
+
+	return 0;
+}
